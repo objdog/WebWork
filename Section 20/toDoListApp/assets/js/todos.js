11f63a12ceb5e69ccb5df8bbe2168c6903f96bf0
Edward Adams <objdog@gmail.com>
2019-01-26 14:03:09 -0800

    Initial push for Section 20

---
+++ Section 20/toDoListApp/assets/js/todos.js
@@ -0,0 +1,21 @@
+//Check off specific todos by clicking
+$("ul").on("click", "li", function() {
+  $(this).toggleClass("complete");
+});
+//Delete todos by clicking on the delete button
+$("ul").on("click", "span", function(event) {
+  $(this).parent().fadeOut(400,function(){
+    $(this).remove();
+  })
+  event.stopPropagation();
+});
+
+$("input[type='text']").keypress(function(event){
+  if (event.which === 13) {
+    //grab the contents of the input and clear the input
+    var toDoText = $(this).val();
+    $(this).val("");
+    //create new li and add to ul
+    $("ul").append("<li><span><i class='fas fa-trash-alt'></i></span>" + toDoText + "</li>")
+  }
+});
