7b14f18df1e9b98a20ef8f5aa1da7277586f4e7b
Ed <objdog@gmail.com>
2019-02-20 21:14:23 +0000

    Just modified the styling of the show page

--- Section 33/YelpCamp/app.js
+++ Section 33/YelpCamp/app.js
@@ -14,6 +14,7 @@ seedDB();
 mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static(__dirname + "/public"));
 
 
 //==============//        
