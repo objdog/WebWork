45dadddf6bb684aed33e1c94942351a2a2bfcc63
Ed <objdog@gmail.com>
2019-02-20 20:06:35 +0000

    Added comment functionality

---
+++ Section 31/RESTful Routing/BlogApp/app.js
@@ -0,0 +1,115 @@
+var bodyParser = require("body-parser"),
+    methodOverride = require("method-override"),
+    mongoose = require("mongoose"),
+    express = require("express"),
+    expressSanitizer = require("express-sanitizer"),
+    app = express();
+// APP SETUP
+mongoose.connect("mongodb://localhost/restful_blog_app", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+mongoose.set("useFindAndModify", false);
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(expressSanitizer());
+app.use(methodOverride("_method"));
+    
+// MONGOOSE / MODEL CONFIG
+var blogSchema = new mongoose.Schema({
+    title: String,
+    image: String,
+    body: String,
+    created: {type: Date, default: Date.now}
+});
+
+var Blog = mongoose.model("Blog", blogSchema);
+
+
+// RESTFUL ROUTES
+
+//-- INDEX ROUTE
+app.get("/", function(req, res) {
+    res.redirect("/blogs");
+})
+app.get("/blogs", function(req, res){
+    Blog.find({}, function(err, blogs){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("index", {blogs: blogs});
+        }
+    });
+    
+});
+
+//-- NEW ROUTE
+
+app.get("/blogs/new", function(req, res) {
+    res.render("new");
+})
+
+//-- CREATE ROUTE
+
+app.post("/blogs", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.create(req.body.blog, function(err, newBlog){
+        if(err){
+            console.log(err);
+        } else{
+            res.redirect("/blogs");
+        }
+    });
+});
+
+//-- SHOW ROUTE
+
+app.get("/blogs/:id", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("show", {blog: foundBlog});
+        }
+    });
+});
+
+//-- EDIT ROUTE
+
+app.get("/blogs/:id/edit", function(req, res) {
+    Blog.findById(req.params.id, function(err, foundBlog){
+        if(err){
+            res.redirect("/blogs");
+        } else{
+            res.render("edit", {blog: foundBlog});
+        }
+    });
+});
+
+//-- UPDATE ROUTE
+
+app.put("/blogs/:id", function(req, res){
+    req.body.blog.body = req.sanitize(req.body.blog.body);
+    Blog.findByIdAndUpdate(req.params.id, req.body.blog, function(err, updatedBlog){
+        if(err){
+            res.redirect('/blogs');
+        } else {
+            res.redirect('/blogs/'+ req.params.id);
+        }
+    });
+});
+
+//-- DELETE ROUTE
+
+app.delete("/blogs/:id", function(req, res){
+    Blog.findByIdAndRemove(req.params.id, function(err){
+        if(err){
+            res.redirect("/blogs");
+        } else {
+            res.redirect("/blogs");
+        }
+    });
+});
+
+// SERVER START
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Blog server is running...");
+})
---
+++ Section 33/YelpCamp/app.js
@@ -0,0 +1,159 @@
+//==============//
+// Server Setup //
+//==============//
+
+var express    = require("express"),
+    app        = express(),
+    bodyParser = require("body-parser"),
+    mongoose   = require('mongoose'),
+    Campground = require('./models/campground'),
+    Comment    = require("./models/comment"),
+    seedDB     = require('./seeds');
+
+seedDB();
+mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({extended: true}));
+
+
+//==============//        
+// ROUTES SETUP //
+//==============//
+
+
+
+//-----------------------//
+// RESTful Routing Table //
+//-----------------------//
+
+/* NAME:     ROUTE:                     TYPE:       DESCRIPTION:
+================================================================================================
+   ROOT        /                        GET         Landing page for the application.
+   INDEX       /campgrounds             GET         Listing of all campsites.
+   NEW         /campgrounds/new         GET         Displays a form for adding a campsite
+   CREATE      /campgrounds             POST        Creates the new site and redirects to INDEX
+   SHOW        /campgrounds/:id         GET         Shows more details about a selected site
+   EDIT        /campgrounds/:id/edit    GET         Populates a form with specific campground info
+   UPDATE      /campgrounds/:id         PUT         Updates the information of a specific campground
+   DELETE      /campgrounds/:id         DELETE      Removes a campground record from the DB.
+   
+==============================================================================================*/
+
+//==================
+// CAMPGROUND ROUTES
+//==================
+
+// GET ROUTES /
+
+app.get("/" , function(req, res){
+    res.render("landing");
+});
+
+app.get("/campgrounds" , function(req, res){
+    // Get all the campgrounds from the db
+    Campground.find({}, function(err, allCampgrounds){
+        if(err){
+            console.log(err);
+        } else{
+            res.render("campgrounds/index", {campgrounds:allCampgrounds})
+        }
+    });
+    //Render the file
+    // res.render("campgrounds", {campgrounds: campgrounds});
+});
+
+app.get("/campgrounds/new" , function(req, res){
+    res.render("campgrounds/new.ejs");
+});
+
+app.get("/campgrounds/:id", function(req, res){
+    Campground.findById(req.params.id).populate("comments").exec(function(err, foundCampground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("campgrounds/show" , {campground: foundCampground});
+        }
+    });
+    
+});
+
+// POST ROUTES //
+
+app.post('/campgrounds' , function(req, res){
+   //get data from form and add to campgrounds array
+   //rediret to campgrounds page.
+   var name = req.body.name;
+   var image = req.body.image;
+   var description = req.body.description;
+   
+   var newCampground = {name: name, image: image, description: description}
+   Campground.create(newCampground, function(err, newlyCreated){
+       if(err){
+           console.log(err);
+       } else {
+           res.redirect("/campgrounds");
+       }
+   });
+});
+
+//================
+// COMMENTS ROUTES
+//================
+
+app.get("/campgrounds/:id/comments/new", function(req, res) {
+    Campground.findById(req.params.id, function(err, campground){
+        if(err){
+            console.log(err);
+        } else {
+            res.render("comments/new", {campground: campground})
+        }
+    });
+});
+
+app.post("/campgrounds/:id/comments", function(req, res){
+    //lookup campground using id
+    Campground.findById(req.params.id, function(err, campground){
+       if(err){
+           console.log(err);
+           res.redirect("/campgrounds");
+       } else {
+           Comment.create(req.body.comment, function(err, comment){
+               if(err){
+                   console.log(err);
+               } else{
+                   campground.comments.push(comment);
+                   campground.save();
+                   res.redirect("/campgrounds/" + campground._id);
+               }
+           });
+       }
+    });
+    //create new commment
+    //connect new comment to campground
+    //redirect to the show page
+});
+
+// PAY NO ATTENTION, DEBUG CODE
+
+// Campground.create(
+//     {
+//         name: "Granite Hill",
+//         image: "https://images.unsplash.com/photo-1504280390367-361c6d9f38f4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+//         description: "This is a huge granite hill, no bathrooms. No water. No Electricity. Beautiful granite!"
+//     },
+//     function(err, campground){
+//         if(err){
+//             console.log(err);
+//         } else {
+//             console.log("Newly Created Campground:");
+//             console.log(campground);
+//         }
+//     });
+
+//==================//
+// START THE SERVER //
+//==================//
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("The YelpCamp server has started.");
+});
