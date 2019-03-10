69ecf2ffdaac98673ddd25b49d7e5b23dc405c5f
Ed <objdog@gmail.com>
2019-03-10 22:34:37 +0000

    Heroku work completed.

--- Section 38/YelpCamp/app.js
+++ Section 38/YelpCamp/app.js
@@ -29,7 +29,7 @@ var commentRoutes   = require("./routes/comments"),
 //==============//
 
 // seedDB(); //Seeds the database with some test data.
-mongoose.connect("mongodb://localhost:27017/yelp_camp", { useNewUrlParser: true });
+mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + "/public"));
