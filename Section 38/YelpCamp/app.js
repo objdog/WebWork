4f12fa86194c59c79794dd8e73f7dce8c74c2127
Ed <objdog@gmail.com>
2019-03-11 22:43:55 +0000

    Styled the edit and delete buttons to fit better with site theme, tidied up the app.js

--- Section 38/YelpCamp/app.js
+++ Section 38/YelpCamp/app.js
@@ -64,30 +64,6 @@ app.use("/campgrounds/:id/comments", commentRoutes);
 app.use("/campgrounds/", campgroudRoutes);
 
 
-
-
-
-//-----------------------//
-// RESTful Routing Table //
-//-----------------------//
-
-/* NAME:     ROUTE:                     TYPE:       DESCRIPTION:
-================================================================================================
-   ROOT        /                        GET         Landing page for the application.
-   INDEX       /campgrounds             GET         Listing of all campsites.
-   NEW         /campgrounds/new         GET         Displays a form for adding a campsite
-   CREATE      /campgrounds             POST        Creates the new site and redirects to INDEX
-   SHOW        /campgrounds/:id         GET         Shows more details about a selected site
-   EDIT        /campgrounds/:id/edit    GET         Populates a form with specific campground info
-   UPDATE      /campgrounds/:id         PUT         Updates the information of a specific campground
-   DELETE      /campgrounds/:id         DELETE      Removes a campground record from the DB.
-   
-==============================================================================================*/
-
-
- 
-
-
 //==================//
 // START THE SERVER //
 //==================//
